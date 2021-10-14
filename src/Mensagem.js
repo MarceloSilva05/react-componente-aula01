05c26e8c782c195bcda7e3b055457a49197047ca
MarceloSilva05 <jmarcelosilva.2021@gmail.com>
2021-10-13 21:11:34 -0300

    primeiro commit

---
+++ src/Mensagem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Mensagem = () => (
+    <div>
+        <pre>{`
+         A nota de 100 tem um
+         peixe pra te lembrar que
+         dinheiro não nasce em
+         árvore. vai trabalhar! 
+        `}</pre>
+    </div>
+);
